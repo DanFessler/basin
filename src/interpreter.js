5827cb61748fb516d30d121cca9b5b51a213ec05
Dan Fessler <fezzinate@gmail.com>
2018-09-16 22:30:52 -0700

    prettier formatting

--- src/interpreter.js
+++ src/interpreter.js
@@ -4,9 +4,7 @@
 // if expression is a literal, no need for the object
 
 interpreter = {
-
   RUN: function(script, init) {
-
     // Mark where the new memory stack begins
     var stackindex = this.Stack.length;
 
@@ -16,7 +14,7 @@ interpreter = {
 
     // iterate over the script
     for (var i = 0; i < script.length; i++) {
-      script[i] = this.EVAL(script[i], null, i+1);
+      script[i] = this.EVAL(script[i], null, i + 1);
     }
 
     // pop the memory stack back to where it was
@@ -27,26 +25,21 @@ interpreter = {
   },
 
   FIND: function(keyword, lineNumber) {
-
     // iterate through all the keywords in the stack
     // starting from the end until we find a match
-    for (var i = this.Stack.length-1; i >= 0; i--) {
+    for (var i = this.Stack.length - 1; i >= 0; i--) {
       if (keyword in this.Stack[i]) {
         return this.Stack[i];
       }
     }
 
     // if no match was found, log an error
-    console.error(
-      "KEY NOT FOUND: '" + keyword +
-      "' on line: " + lineNumber
-    );
+    console.error("KEY NOT FOUND: '" + keyword + "' on line: " + lineNumber);
 
     return null;
   },
 
   EVAL: function(expression, init, line) {
-
     // if expression is literal, return itself as the value
     if (!expression || typeof expression != "object") return expression;
 
@@ -70,104 +63,146 @@ interpreter = {
 
     // Execute keyword from the stack with the computed params
     var match = this.FIND(keyword, lineNumber);
-    if (match) return typeof match[keyword] == "function" ? match[keyword].apply( this, params ) : match[keyword];
+    if (match)
+      return typeof match[keyword] == "function"
+        ? match[keyword].apply(this, params)
+        : match[keyword];
     else return keyword;
   },
 
   Stack: [
-    { LET: function(key, value) {
-      var obj = {};
-      obj[key] = value;
-      this.Stack.push(obj);
-    }},
+    {
+      LET: function(key, value) {
+        var obj = {};
+        obj[key] = value;
+        this.Stack.push(obj);
+      }
+    },
 
-    { FUNCTION: function(key) {
+    {
+      FUNCTION: function(key) {
+        //console.log(arguments[2]);
 
-      //console.log(arguments[2]);
+        var args = Array.prototype.slice.call(arguments);
+        var params = args.slice(1, args.length - 1);
+        var script = args[args.length - 1];
 
-      var args = Array.prototype.slice.call(arguments);
-      var params = args.slice(1,args.length-1);
-      var script = args[args.length-1];
+        var obj = {};
+        obj[key] = function() {
+          this.EVAL(
+            script,
+            function(key, value) {
+              this.FIND("LET")["LET"].bind(this)(key, value);
+            }.bind(this, args[1], arguments[0])
+          );
+        };
 
-      var obj = {};
-      obj[key] = function() {
-        this.EVAL(script, function(key, value){
-          this.FIND('LET')['LET'].bind(this)(key, value);
-        }.bind(this, args[1], arguments[0]));
-      };
-
-      this.Stack.push(obj);
-    }},
+        this.Stack.push(obj);
+      }
+    },
 
     // Assignment operators
-    { SET: function(key, value) {
-      var variable = this.FIND(key);
-      variable[key] = value;
-    }},
-    { INC: function(key) {
-      var variable = this.FIND(key);
-      variable[key]++
-    }},
+    {
+      SET: function(key, value) {
+        var variable = this.FIND(key);
+        variable[key] = value;
+      }
+    },
+    {
+      INC: function(key) {
+        var variable = this.FIND(key);
+        variable[key]++;
+      }
+    },
 
     // Arithmetic operators
-    { 'ADD': function(A, B) {
-      return A + B;
-    }},
-    { 'SUB': function(A, B) {
-      return A - B;
-    }},
-    { 'MUL': function(A, B) {
-      return A * B;
-    }},
-    { 'DIV': function(A, B) {
-      return A / B;
-    }},
-    { 'MOD': function(A, B) {
-      return A % B;
-    }},
+    {
+      ADD: function(A, B) {
+        return A + B;
+      }
+    },
+    {
+      SUB: function(A, B) {
+        return A - B;
+      }
+    },
+    {
+      MUL: function(A, B) {
+        return A * B;
+      }
+    },
+    {
+      DIV: function(A, B) {
+        return A / B;
+      }
+    },
+    {
+      MOD: function(A, B) {
+        return A % B;
+      }
+    },
 
     // Relational operators
-    { '==': function(A, B) {
-      return A == B;
-    }},
-    { '<>': function(A, B) {
-      return A != B;
-    }},
-    { '>': function(A, B) {
-      return A > B;
-    }},
-    { '<': function(A, B) {
-      return A < B;
-    }},
-    { '>=': function(A, B) {
-      return A >= B;
-    }},
-    { '<=': function(A, B) {
-      return A <= B;
-    }},
+    {
+      "==": function(A, B) {
+        return A == B;
+      }
+    },
+    {
+      "<>": function(A, B) {
+        return A != B;
+      }
+    },
+    {
+      ">": function(A, B) {
+        return A > B;
+      }
+    },
+    {
+      "<": function(A, B) {
+        return A < B;
+      }
+    },
+    {
+      ">=": function(A, B) {
+        return A >= B;
+      }
+    },
+    {
+      "<=": function(A, B) {
+        return A <= B;
+      }
+    },
 
     // Logical operators
-    { 'AND': function(A, B) {
-      return A && B;
-    }},
-    { 'OR': function(A, B) {
-      return A || B;
-    }},
-
-
-    { PRINT: function() {
-      var string = '';
-      for (var i = 0; i < arguments.length; i++) {
-      string += arguments[i] != null ? arguments[i] : '';
+    {
+      AND: function(A, B) {
+        return A && B;
       }
-      console.log(string);
-    }},
-    { REPEAT: function(count, script) {
-      for (var i=0; i<count; i++) {
-      this.EVAL(script);
+    },
+    {
+      OR: function(A, B) {
+        return A || B;
       }
-    }},
+    },
+
+    {
+      PRINT: function() {
+        var string = "";
+        for (var i = 0; i < arguments.length; i++) {
+          string += arguments[i] != null ? arguments[i] : "";
+        }
+        console.log(string);
+      }
+    },
+    {
+      REPEAT: function(count, script) {
+        for (var i = 0; i < count; i++) {
+          this.EVAL(script);
+        }
+      }
+    }
   ]
-}
+};
 
-module.exports = interpreter
+module.exports = interpreter;
