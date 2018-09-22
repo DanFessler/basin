ec3e66b8ae7cba009399833accef421194a85a35
Dan Fessler <fezzinate@gmail.com>
2018-09-21 23:32:25 -0700

    reverting demo program

--- demo/index.js
+++ demo/index.js
@@ -1,31 +1,25 @@
 var interpreter = require("../src/interpreter");
 
 let program = [
-  // {
-  //   FUNCTION: ["GREET", "name"],
-  //   script: [{ PRINT: { ADD: ["Hello ", { name: null }] } }]
-  // },
-  // { GREET: "World!" },
-
-  // {
-  //   FOR: ["i", 1, 10, 1],
-  //   script: [{ PRINT: { i: null } }]
-  // }
-
-  // { SET: ["i", 0] },
-  // {
-  //   WHILE: null,
-  //   script: [
-  //     [{ "<": [{ i: null }, 20] }],
-  //     [{ PRINT: { i: null } }, { SET: ["i", { ADD: [{ i: null }, 1] }] }]
-  //   ]
-  // }
-
   {
     FUNCTION: ["GREET", "name"],
     script: [{ PRINT: { ADD: ["Hello ", { name: null }] } }]
   },
-  { GREET: "Dan" }
+  { GREET: "World!" },
+
+  {
+    FOR: ["i", 1, 10, 1],
+    script: [{ PRINT: { i: null } }]
+  }
+
+  { SET: ["i", 0] },
+  {
+    WHILE: null,
+    script: [
+      [{ "<": [{ i: null }, 20] }],
+      [{ PRINT: { i: null } }, { SET: ["i", { ADD: [{ i: null }, 1] }] }]
+    ]
+  }
 ];
 
 interpreter.RUN(program);
