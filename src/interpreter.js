DanFessler/basin

commit cda403144b65fb1fffae0238e6fc45db92282959

BUGFIX: allow multiple params on functions

src/interpreter.js
@@ -82,8 +82,6 @@ interpreter = {
 
     {
       FUNCTION: function(key) {
-        //console.log(arguments[2]);
-
         var args = Array.prototype.slice.call(arguments);
         var params = args.slice(1, args.length - 1);
         var script = args[args.length - 1];
@@ -93,8 +91,10 @@ interpreter = {
           this.EVAL(
             script,
             function(key, value) {
-              this.FIND("LET")["LET"].bind(this)(key, value);
-            }.bind(this, args[1], arguments[0])
+              for (let i = 0; i < key.length; i++) {
+                this.FIND("LET")["LET"].bind(this)(key[i], value[i]);
+              }
+            }.bind(this, params, [...arguments])
           );
         };
 

demo/index.js
@@ -1,13 +1,31 @@
-var interpreter = require('../src/interpreter')
+var interpreter = require("../src/interpreter");
 
 let program = [
+  // {
+  //   FUNCTION: ["GREET", "name"],
+  //   script: [{ PRINT: { ADD: ["Hello ", { name: null }] } }]
+  // },
+  // { GREET: "World!" },
+
+  // {
+  //   FOR: ["i", 1, 10, 1],
+  //   script: [{ PRINT: { i: null } }]
+  // }
+
+  // { SET: ["i", 0] },
+  // {
+  //   WHILE: null,
+  //   script: [
+  //     [{ "<": [{ i: null }, 20] }],
+  //     [{ PRINT: { i: null } }, { SET: ["i", { ADD: [{ i: null }, 1] }] }]
+  //   ]
+  // }
+
   {
-    "FUNCTION": ["GREET", "name"],
-    "script": [
-      { "PRINT": { "ADD": ["Hello ", {"name": null}] } }
-    ]
+    FUNCTION: ["GREET", "name"],
+    script: [{ PRINT: { ADD: ["Hello ", { name: null }] } }]
   },
-  { "GREET": "World!" }
-]
+  { GREET: "Dan" }
+];
 
 interpreter.RUN(program);

commit 53bb1480e3245501d140657cf093342dc9157c56

Resubmitting bugfix cda4031
accidentally stomped it on merge

src/interpreter.js
@@ -92,8 +92,10 @@ interpreter = {
             this.EVAL(
               script,
               function(key, value) {
-                this.FIND("LET")["LET"].bind(this)(key, value);
-              }.bind(this, args[1], arguments[0])
+                for (let i = 0; i < key.length; i++) {
+                  this.FIND("LET")["LET"].bind(this)(key[i], value[i]);
+                }
+              }.bind(this, params, [...arguments])
             );
           } catch (value) {
             return value;
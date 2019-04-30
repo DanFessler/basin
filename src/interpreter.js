53bb1480e3245501d140657cf093342dc9157c56
DanFessler <fezzinate@gmail.com>
2019-04-30 16:39:58 -0700

    Resubmitting bugfix cda4031
    accidentally stomped it on merge

--- src/interpreter.js
+++ src/interpreter.js
@@ -92,8 +92,10 @@ interpreter = {
             this.EVAL(
               script,
               function(key, value) {
-                this.FIND("LET")["LET"].bind(this)(key, value);
-              }.bind(this, args[1], arguments[0])
+                for (let i = 0; i < key.length; i++) {
+                  this.FIND("LET")["LET"].bind(this)(key[i], value[i]);
+                }
+              }.bind(this, params, [...arguments])
             );
           } catch (value) {
             return value;
